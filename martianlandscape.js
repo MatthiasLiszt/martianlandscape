81bef3d8a19e5863a7548b449e699b882686a189
MatthiasLiszt <mliszt.program@gmail.com>
2017-10-17 16:30:28 +0200

    all mountains

--- martianlandscape.js
+++ martianlandscape.js
@@ -5,7 +5,9 @@ function martianlandscape(screens,pixelSize){
  
  for(var curScreen=0;curScreen<screens;++curScreen)   
   {if(curScreen!=landingSite)
-    {
+    {var landType=Math.floor(Math.random()*5);
+
+      
     }
    else
     {
@@ -14,35 +16,68 @@ function martianlandscape(screens,pixelSize){
   
 }
 
-function makeMountain(size,screenSize)
+function makeMountain(size,screenSize,offset)
  {var curSize;
   var x,y,p1x,p2x,p1y,p2y;
   var plusY;
+  var of=0;
  
   console.log("executing makeMountain");
   switch(size){
    case 'small':
     curSize=screenSize/3;
     plusY=screenSize/2;
+    of=curSize*offset; 
     break; 
    case 'medium':
-    curSize=screenSize/2;
+    curSize=screenSize/2.3;
+    plusY=screenSize/2;
+    of=curSize*offset;
     break;  
    case 'large':
     curSize=screenSize;
+    plusY=screenSize/4;
+    of=curSize*offset;
     break;  
   }
   x=Math.round(curSize/2);
   curSize=Math.round(curSize);
-  p1x=Math.round(Math.random()*x);
-  p1y=Math.round(Math.random()*curSize);
+  p1x=Math.round(of+Math.random()*x);
+  p1y=(Math.round(Math.random()*curSize))%160+plusY;
   p2x=p1x+Math.round(Math.random()*x);
-  p2y=Math.round(Math.random()*curSize); 
+  p2y=(Math.round(Math.random()*curSize))%160+plusY;  
+   
   
-  var code="<path stroke='black' d='M 0 300 l "+p1x+" "+p1y+" l "+p2x+" "+p2y+" l "+curSize+" 300' />";
+  var code="<path stroke='none' fill='brown' d='M "+of+" 300 L "+p1x+" "+p1y+" L "+p2x+" "+p2y+" L "+(curSize+of)+" 300' />";
+  console.log(code);
   return code;
  }
 
-function makeFlatLand(color,size,screenSize)
- {
+function makeFlatLand(color,size,screenSize,offset)
+ {var curSize;
+  //var x,y,p1x,p2x,p1y,p2y;
+  var plusY=160;
+  var of=0;
+
+  console.log("executing makeFlatLand");
+  switch(size){
+  case 'small': curSize=screenSize/3;
+                plusY=screenSize/2;
+                of=curSize*offset;
+                break;
+   case 'medium':
+                 curSize=screenSize/2;
+                 plusY=screenSize/2;
+                 of=curSize*offset;
+                 break;  
+   case 'large':
+                curSize=screenSize;
+                plusY=screenSize/2;
+                of=curSize*offset;   
+    break;
+  }
+
+  var code="<path stroke-width='5' stroke='"+color+"' d='M "+of+" 300 L "+(curSize+of)+" 300' />";
+  console.log(code);
+  return code;
  } 
